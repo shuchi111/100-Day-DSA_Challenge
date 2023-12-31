ed09fdfe864dd6dafb6014d9cb6286c95c3fa769
Shuchi Shukla <shuchis9999@gmail.com>
2023-12-31 17:05:35 +0530

    day 11

---
+++ Day 12/LRU cache.cpp
@@ -0,0 +1,25 @@
+//Day 12 LRU cache
+/*
+
+Example 1:
+
+Input:
+cap = 2
+Q = 2
+Queries = SET 1 2 GET 1
+Output: 2
+Explanation: 
+Cache Size = 2
+
+SET 1 2 GET 1
+SET 1 2 : 1 -> 2
+
+GET 1 : Print the value corresponding
+to Key 1, ie 2.
+
+
+
+
+
+
+*/
