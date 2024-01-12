83b4285f179a3ff3616dc117a214cabe4f699592
Shuchi Shukla <107612618+shuchi111@users.noreply.github.com>
2024-01-12 05:32:18 +0530

    potd

---
+++ Day 24/Reverse First K elements of Queue.cc
@@ -0,0 +1,64 @@
+//Reverse First K elements of Queue
+
+//Solution
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution
+{
+    public:
+    
+    // Function to reverse first k elements of a queue.
+    queue<int> modifyQueue(queue<int> q, int k) {
+        // add code here.
+        stack<int>st;
+        queue<int>res;
+        while(k--){
+            st.push(q.front());
+            q.pop();
+        }
+        while(!st.empty()){
+            res.push(st.top());
+            st.pop();
+        }
+        while(!q.empty()){
+            res.push(q.front());
+            q.pop();
+        }
+        return res;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t-- > 0) {
+        int n, k;
+        cin >> n >> k;
+        queue<int> q;
+        while (n-- > 0) {
+            int a;
+            cin >> a;
+            q.push(a);
+        }
+        Solution ob;
+        queue<int> ans = ob.modifyQueue(q, k);
+        while (!ans.empty()) {
+            int a = ans.front();
+            ans.pop();
+            cout << a << " ";
+        }
+        cout << endl;
+    }
+}
+// } Driver Code Ends
