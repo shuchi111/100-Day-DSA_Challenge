e870fa4480cd683f42296bbabece693c91c01ba9
Shuchi Shukla <107612618+shuchi111@users.noreply.github.com>
2024-01-16 21:50:08 +0530

    leetcode potd

---
+++ Day 28/380. Insert Delete GetRandom.cc
@@ -0,0 +1,20 @@
+// 380. Insert Delete GetRandom O(1)
+/*
+
+Example 1:
+
+Input
+["RandomizedSet", "insert", "remove", "insert", "getRandom", "remove", "insert", "getRandom"]
+[[], [1], [2], [2], [], [1], [2], []]
+Output
+[null, true, false, true, 2, true, false, 2]
+
+
+
+Approach 
+  
+
+
+*/
+
+//Solution 
