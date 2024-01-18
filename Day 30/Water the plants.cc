2780e62dfb514623efa2d4fd2d1269d90e6bc69d
Shuchi Shukla <107612618+shuchi111@users.noreply.github.com>
2024-01-18 13:24:49 +0530

    Create Water the plants.cc

---
+++ Day 30/Water the plants.cc
@@ -0,0 +1,19 @@
+// Water the plants
+
+/*
+
+Input:
+n = 6
+gallery[] = {-1, 2, 2, -1, 0, 0}
+Output:
+2
+Explanation: 
+Sprinklers at index 2 and 5
+can water the full gallery, span of
+sprinkler at index 2 = [0,4] and span
+of sprinkler at index 5 = [5,5].
+
+
+*/
+
+//solution
