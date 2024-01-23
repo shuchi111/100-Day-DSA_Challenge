1216b6543cf56fef144230f8ab405034fa312269
Shuchi Shukla <107612618+shuchi111@users.noreply.github.com>
2024-01-23 20:30:06 +0530

    leetcode potd

--- Day 35/1239. Maximum Length of a Concatenated String with Unique Characters.py
+++ Day 35/1239. Maximum Length of a Concatenated String with Unique Characters.py
@@ -36,17 +36,28 @@ The time complexity of this recursive solution is exponential, O(2^n), where n i
 """
 class Solution:
     def maxLength(self, a: List[str]) -> int:
+        # Recursive helper function to explore all possible combinations
         def f(i, s):
+            # Base Case: If i reaches the length of a, return the length of the set s
             if i == len(a):
                 return len(s)
 
+            # Recursive Exploration: Call f(i+1, s) to explore the case where the current string is not included
             result = f(i+1, s)
+
+            # Include Current String (a[i]):
+            # Create a new set g by taking the union of s and the set of characters in the current string a[i]
             g = s | {*a[i]}
+
+            # If len(g) is equal to the sum of the lengths of s and a[i], update the result
             if len(g) == len(s) + len(a[i]):
+                # Recursively call f(i+1, g) to explore the case where the current string is included
                 result = max(result, f(i+1, g))
 
             return result
         
+        # Call the recursive helper function with initial parameters (starting from index 0 and an empty set)
         return f(0, set())
+
 
   
